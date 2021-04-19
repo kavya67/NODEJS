d32ad55a2a400a64957d22ea46b9472955057e9b
kavya67 <kavyamaniyan13@gmail.com>
2021-04-19 13:42:12 +0530

    updated script in package json file

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  res.json({
-    name: "kavya",
-  });
-});
 
 app.use("/product", (req, res, next) => {
   res.json({
     say: "Hello",
   });
 });
+
+app.use("/", (req, res, next) => {
+    res.json({
+      name: "kavya",
+    });
+  });
+  
 app.listen(3000);
