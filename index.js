c7d8732a2bd527c88406666325c51be47834e085
kavya <kavya.techmakers@gmail.com>
2021-04-20 14:47:50 +0530

    Added mongo db driver

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-const rootDir = require("./utilities/path");
+// const path = require("path");
+// const rootDir = require("./utilities/path");
+require('dotenv').config()
+
+const mongoConnect = require("./utilities/database");
 
 const app = express();
 
 app.use(express.urlencoded());
 app.use(cors());
 
-const admin = require("./routes/admin");
-const home = require("./routes/home");
-app.use("/admin", admin);
-app.use("/", home);
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-});
+// const admin = require("./routes/admin");
+// const home = require("./routes/home");
+// app.use("/admin", admin);
+// app.use("/", home);
+// app.use((req, res, next) => {
+//   res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+// });
 
-app.listen(3000);
+// app.listen(3000);
+
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
